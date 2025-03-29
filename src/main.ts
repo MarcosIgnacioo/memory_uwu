4a763f612ccc5ddb7cf937c7567d63619e9d1d94
MarcosIgnacioo <magma_ruby145@hotmail.com>
2025-03-29 08:51:47 -0700

    uwuwuoadsfjkasdjf

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,26 @@
 import './style.css'
 import p5 from 'p5'
 
-const WIDTH = 800
+const WIDTH = 1200
 const HEIGHT = 800
+const PAIRS = 10
+const PADDING = 10
+const CARD_W = (WIDTH) / (PAIRS) - PADDING
+const CARD_H = HEIGHT / 5
 //const total_tiles = 8
 //const tile_width = WIDTH / total_tiles
 
 
+interface Vector2 {
+  x: number;
+  y: number;
+}
 
 interface State {
-  first_pokemon: string
-  second_pokemon: string
+  prev_selection: number
+  current_selection: number
   is_correct: boolean
+  click_position: Vector2
   pokemon_list: Pokemon[]
 }
 
@@ -21,7 +30,7 @@ interface Pokemon {
   sprite: string
   types: TypeSlot[]
   is_guessed: boolean
-  is_being_guess: boolean
+  is_hidden: boolean
   image: p5.Image
 }
 
@@ -39,7 +48,7 @@ interface Type {
 async function get_pokemon_list(limit: number): Promise<Pokemon[]> {
   const pokemon_array: Pokemon[] = [];
   for (let i = 0; i < limit; i++) {
-    const id_fetch = Math.trunc(Math.random() * 100 + 1)
+    const id_fetch = Math.trunc(Math.random() * 800 + 1)
     const url = `https://pokeapi.co/api/v2/pokemon/${id_fetch}`
     const response = await fetch(url)
     const { sprites, name, id, types } = await response.json()
@@ -49,7 +58,7 @@ async function get_pokemon_list(limit: number): Promise<Pokemon[]> {
       id: id,
       types: types,
       sprite: sprites.front_default,
-      is_being_guess: false,
+      is_hidden: true,
       is_guessed: false
     }
     pokemon_array.push(pokemon);
@@ -58,7 +67,7 @@ async function get_pokemon_list(limit: number): Promise<Pokemon[]> {
 }
 
 async function init_game_state(state: State) {
-  const pokemon_list = await get_pokemon_list(10)
+  const pokemon_list = await get_pokemon_list(PAIRS)
   state.pokemon_list = pokemon_list
 }
 
@@ -67,13 +76,13 @@ const sketch = (p: p5): any => {
 
 
   const game_state: State = {
+    prev_selection: 0,
+    current_selection: 0,
+    click_position: { x: 0, y: 0 },
+    is_correct: false,
     pokemon_list: []
   }
 
-  interface Vector2 {
-    x: number;
-    y: number;
-  }
 
   p.preload = async function() {
     //const img = p.loadImage('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/21.png')
@@ -88,16 +97,48 @@ const sketch = (p: p5): any => {
     p.createCanvas(WIDTH, HEIGHT);
   }
 
+  function get_random_rgb(): number[] {
+    return [Math.random() * 255, Math.random() * 255, Math.random() * 255]
+  }
   p.draw = function() {
     p.background(255)
-    for (let i = 0; i < game_state.pokemon_list.length; i++) {
-      const pokemon = game_state.pokemon_list[i];
-      console.log(pokemon);
-      p.image(pokemon.image, i * WIDTH / 10, 10)
+    draw_text(`Clicked on: (${game_state.click_position.x} , ${game_state.click_position.y}) `, WIDTH / 3, HEIGHT / 2)
+    const y = 10
+    if (game_state.pokemon_list.length) {
+      for (let x = 0; x < game_state.pokemon_list.length; x++) {
+        const pokemon = game_state.pokemon_list[x];
+        if (pokemon.is_hidden) {
+          p.fill("#ff2756");
+          p.stroke("#ccffff")
+          p.strokeWeight(4);
+          p.rect((x * CARD_W) + (PADDING * x), y, CARD_W, CARD_H)
+        } else {
+          p.image(pokemon.image, x * CARD_W, CARD_H)
+        }
+      }
+    } else {
+      draw_text("LOADING...", WIDTH / 2, HEIGHT / 2);
     }
   }
 
+  function draw_text(text: string, x: number, y: number) {
+    const og_stroke = p.STROKE
+    const og_weight = 0
+    p.textSize(24)
+    p.stroke(5)
+    p.strokeWeight(1)
+    p.fill(0)
+    p.text(text, x, y);
+    p.stroke(og_stroke)
+    p.strokeWeight(og_weight)
+  }
 
-  p.mouseClicked = function() { }
+  p.mouseClicked = function() {
+    game_state.click_position = { x: p.mouseX, y: p.mouseY };
+    const { x, y } = game_state.click_position;
+    for (let i = 0; i < game_state.pokemon_list.length; i++) {
+      const pokemon = game_state.pokemon_list[i];
+    }
+  }
 }
 new p5(sketch);
